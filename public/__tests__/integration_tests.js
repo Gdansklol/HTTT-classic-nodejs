d4fca06852ad5dbdbe98ccc53edbc5b59d5ee23b
Gdansklol <swedenebomi@gmail.com>
2023-01-07 21:59:46 +0100

    level-1 and level-2

---
+++ public/__tests__/integration_tests.js
@@ -0,0 +1,11 @@
+/**
+ * @group integration
+ */
+
+ describe('jest', () => {
+    test('component test', () => {
+      it('should work', () => {
+        expect(1).toBe(1);
+      });
+    });
+  });
---
+++ public/__tests__/unit_tests.js
@@ -0,0 +1,11 @@
+/**
+ * @group unit
+ */
+
+ describe('jest', () => {
+    describe('unit test', () => {
+      it('should be work', () => {
+        expect(1).toBe(1);
+      });
+    });
+  });
