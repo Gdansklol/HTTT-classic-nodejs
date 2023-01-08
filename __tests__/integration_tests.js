75df58ab2b2193d11fd725b64ed96c54e072c3d3
Gdansklol <swedenebomi@gmail.com>
2023-01-08 01:13:20 +0100

    level-2

---
+++ __tests__/integration_tests.js
@@ -0,0 +1,11 @@
+/**
+ * @group integrarion
+ */
+
+describe('jest',()=>{
+    describe('integrion test',()=>{
+        test('shoild be work',()=>{
+            expect(1).toBe(1);
+        });
+    });
+});
